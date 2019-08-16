cdf8c4638de052420d203fe0bbbce773591d876a
Jade Lopez <47537434+jclairelopez@users.noreply.github.com>
2019-08-16 11:25:27 -0500

    first commit

---
+++ Package development.R
@@ -0,0 +1,15 @@
+
+# Why make a package?
+# test, generalize, document, share
+
+install.packages("usethis")
+
+# Create a package on your desktop
+#usethis::create_package("~/Desktop/mypackage")
+
+# Create package in RStudio.cloud (within the main project)
+usethis::create_package("mypackage")
+
+# can use build tools from within an active project: testing, checking, building source, etc.
+# description
+Package 
